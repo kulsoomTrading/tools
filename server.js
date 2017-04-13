0e48a683b593199466767d525582e7e8d33c7e1a
Gheric Speiginer <gheric.speiginer@gmail.com>
2017-04-13 12:00:16 -0400

    Disabled https in local dev server until argon-app can correctly handle unsigned certs

--- server.js
+++ server.js
@@ -8,11 +8,11 @@ const httpPort = process.env.PORT||1337;
 pem.createCertificate({ days:1, selfSigned:true }, function(err, keys) {
     // Create middleware for a static server
     var app = express();
-    app.all('*', (req, res, next) => {
-        // always redirect to https
-        if (req.secure) return next();
-        res.redirect('https://' + req.hostname + ':' + httpsServer.address().port + req.url);
-    });
+    // app.all('*', (req, res, next) => {
+    //     // always redirect to https
+    //     if (req.secure) return next();
+    //     res.redirect('https://' + req.hostname + ':' + httpsServer.address().port + req.url);
+    // });
     app.use(express.static('.'));
 
     // Create an HTTPS service
