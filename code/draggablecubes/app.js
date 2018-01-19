6da45a3cc05a9fcc8c16634afceb535b1b7aace8
Gheric Speiginer <gheric.speiginer@gmail.com>
2018-01-18 19:29:12 -0500

    Update app.js

--- code/draggablecubes/app.js
+++ code/draggablecubes/app.js
@@ -527,15 +527,6 @@ app.renderEvent.addEventListener(function (frame) {
     renderer.setPixelRatio(app.suggestedPixelRatio);
     var viewport = view.viewport;
     hud.setSize(viewport.width, viewport.height);
-    // if the viewport width and the renderwidth are different
-    // we assume we are rendering on a different surface than
-    // the main display, so we reset the pixel ratio to 1
-    if (viewport.width != view.renderWidth) {
-        renderer.setPixelRatio(1);
-    }
-    else {
-        renderer.setPixelRatio(window.devicePixelRatio);
-    }
     // there is 1 subview in monocular mode, 2 in stereo mode
     for (var _i = 0, _a = app.view.subviews; _i < _a.length; _i++) {
         var subview = _a[_i];
